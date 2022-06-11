61604ff6598ceaa4483b45cc196c410d4121a4f8
Luan Diniz <luanminasgerais@hotmail.com>
2022-06-11 03:08:33 -0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\nScript para compilar automaticamente o jogo de exemplo\n"
+
+gcc game.c -o game -lallegro -lallegro_font -lallegro_primitives -lallegro_audio -lallegro_acodec -lallegro_image
+./game
+
+if [ $? != 0 ]; then
+  echo -e "\nHouve um erro de compilação, revise o log!"
+  
+fi
+echo -e "\nBuild concluída com sucesso!"
